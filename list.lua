8710fe0202b454405b3e184cc419d9ee247bbb5c
ନାମ ନାହିଁ <devj35118@gmail.com>
2025-11-16 01:27:44 -0300

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,6 +1,8 @@
-	[34205921] = { -- Dono John
+return {
+		[34205921] = { -- Dono John
 		Nome = "Dark Ghosts ダークゴースト",
 		Color1 = Color3.fromRGB(255, 255, 255),
 		Color2 = Color3.fromRGB(0, 0, 0),
 		Powers = {"Kick"}
-	}
+		}
+}
